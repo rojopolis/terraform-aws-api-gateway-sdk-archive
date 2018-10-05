8335c477676edee8a4d757a2e18f2a875c41a417
Robert Jordan <rjordan@deba.cl>
2018-10-05 11:52:14 -0700

    outputs

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "archive_file" {
+    description = "Path to archive file"
+    value       = "${var.archive_file}"
+}
--- samples/api_gateway_sdk_archive.tf
+++ samples/api_gateway_sdk_archive.tf
@@ -1,7 +1,11 @@
 module "javascript_api_archive" {
     source         = ".."
-    api_gateway_id = "abc123"
-    stage_name     = "test"
+    api_gateway_id = "12345"
+    stage_name     = "rj-test"
     sdk_type       = "javascript"
     archive_file   = "sdk.zip"
+}
+
+output "archive_file" {
+    value = "${module.javascript_api_archive.archive_file}"
 }
